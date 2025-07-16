52de0add770018b17430c830b93ba624734874b0
Ranga Bandara <rangabandara1729@gmail.com>
2025-07-16 23:00:46 +0530

    inngest.js changed

--- config/inngest.js
+++ config/inngest.js
@@ -10,13 +10,13 @@ export const syncUserCreation = inngest.createFunction(
   { id: 'sync-user-from-clerk' },
   { event: 'clerk/user.created' },
   async({event}) => {
-    const { id, first_name, last_name, email_address, image_url} = event.data;
-    console.log("event.data: " + event.data);
+    const user = event.data;
+    console.log("event.data: " + user);
     const userData = {
-      _id : id,
-      email : email_address[0].email_address,
-      name: first_name + " " + last_name,
-      imageUrl: image_url
+      _id : user.id,
+      email : user.email_addresses?.[0]?.email_address ?? "",
+      name: `${user.first_name ?? ""} ${user.last_name ?? ""}`.trim(),
+      imageUrl: user.image_url ?? ""
     }
     await connectDB();
     await User.create(userData);
@@ -28,13 +28,13 @@ export const syncUserUpdation = inngest.createFunction(
   { id: 'update-user-from-clerk' },
   { event: 'clerk/user.updated' },
   async({event}) => {
-    const { id, first_name, last_name, email_address, image_url} = event.data;
-    console.log("event.data: " + event.data);
+    const user = event.data;
+    console.log("event.data: " + user);
     const userData = {
-      _id : id,
-      email : email_address[0].email_address,
-      name: first_name + " " + last_name,
-      imageUrl: image_url
+      _id : user.id,
+      email : user.email_addresses?.[0]?.email_address ?? "",
+      name: `${user.first_name ?? ""} ${user.last_name ?? ""}`.trim(),
+      imageUrl: user.image_url ?? ""
     }
     await connectDB();
     await User.findByIdAndUpdate(id, userData);
@@ -46,9 +46,9 @@ export const syncUserDeletion = inngest.createFunction(
   { id: 'delete-user-with-clerk' },
   { event: 'clerk/user.deleted' },
   async({event}) => {
-    const { id } = event.data;
-    console.log("event.data: " + event.data);
+    const user = event.data;
+    console.log("event.data: " + user);
     await connectDB();
-    await User.findByIdAndDelete(id);
+    await User.findByIdAndDelete(user.id);
   }
 )
